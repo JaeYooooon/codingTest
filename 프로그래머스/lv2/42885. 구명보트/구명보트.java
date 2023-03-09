0b9270bd8f8144f1b5a0a6987f423e1d40d6e6ba
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-03-09 20:23:16 +0900

    [level 2] Title: 구명보트, Time: 9.23 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -4,7 +4,8 @@ class Solution {
     public int solution(int[] people, int limit) {
         Arrays.sort(people);  // 오름차순 정렬
         int count = 0;
-        int left = 0, right = people.length - 1;
+        int left = 0;
+        int right = people.length - 1;
         while (left <= right) {
             if (people[left] + people[right] <= limit) {
                 left++;
