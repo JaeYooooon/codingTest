a69dd11f5bf163586a0d489e465e3c49a920a546
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-03-09 20:03:09 +0900

    [level 2] Title: 전화번호 목록, Time: 261.71 ms, Memory: 96.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Solution {
+    public boolean solution(String[] phone_book) {
+        // 문자열 배열을 정렬
+        Arrays.sort(phone_book);
+
+        // 인접한 두 문자열을 비교
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                // 다음 문자열이 현재 문자열의 접두어인 경우 false를 반환
+                return false;
+            }
+        }
+
+        // 모든 문자열을 비교했을 때 접두어가 없는 경우 true를 반환
+        return true;
+    }
+}
