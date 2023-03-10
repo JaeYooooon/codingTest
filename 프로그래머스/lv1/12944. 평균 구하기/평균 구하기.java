c08b02a25fe3882c0bb4e6bc383ab5f0d2a1aa47
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-03-10 14:25:12 +0900

    [level 1] Title: 평균 구하기, Time: 2.39 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -1,12 +1,7 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public double solution(int[] arr) {
-        double answer = 0;
-        double res = 0;
-        int len = arr.length;
-        for(int i = 0; i < arr.length; i++){
-            res += arr[i];
-        }
-        answer = res/len;
-        return answer;
+        return IntStream.of(arr).average().orElse(0.0);
     }
 }
