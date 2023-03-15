1b1efd95dcda497796470f8b5e977d4b6680c257
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-03-15 15:59:31 +0900

    [level 2] Title: 올바른 괄호, Time: 19.46 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,10 +1,7 @@
 import java.util.Stack;
-
 class Solution {
     boolean solution(String s) {
-
         Stack<Integer> stack = new Stack<>();
-
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (c == '(') {
@@ -17,7 +14,6 @@ class Solution {
                 stack.pop();
             }
         }
-
         return stack.isEmpty();
     }
 }
