8d923b3812a801869c7d38d4b7bfb94c8eaa50e3
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-03-17 13:32:21 +0900

    [level 2] Title: 프린터, Time: 0.85 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.java
+++ 프로그래머스/lv2/42587. 프린터/프린터.java
@@ -1,44 +1,35 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
-public class Solution {
-    class Task{
-        int location;
-        int priority;
-        public Task(int location, int priority) {
-            this.location = location;
-            this.priority = priority;
-        }
-    }
+class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0;
-
-        Queue<Task> queue = new LinkedList<>();
-
-        for(int i=0; i<priorities.length; i++){
-            queue.add(new Task(i, priorities[i]));
+        int answer = 0; // 인쇄한 문서 수
+        Queue<Integer> queue = new LinkedList<>(); // 대기열을 구현하기 위한 Queue
+        for (int priority : priorities) { // 대기열 초기화
+            queue.add(priority);
         }
+        Arrays.sort(priorities); // 우선순위 배열을 오름차순으로 정렬
 
-        int now=0;
-        while(!queue.isEmpty()){
-            Task cur = queue.poll();
-            boolean flag = false;
-            for(Task t : queue){
-                if(t.priority>cur.priority){
-                    flag = true;
+        int index = priorities.length - 1; // 우선순위 배열에서 가장 높은 우선순위의 인덱스
+        while (!queue.isEmpty()) { // 모든 문서가 인쇄될 때까지 반복
+            int priority = queue.poll(); // 대기열에서 맨 앞의 문서를 꺼냄
+            if (priority == priorities[index]) { // 현재 문서의 우선순위가 가장 높은 경우
+                index--; // 다음으로 가장 높은 우선순위를 가진 문서를 찾기 위해 index를 감소시킴
+                answer++; // 인쇄한 문서 수 증가
+                if (location == 0) { // 요청한 문서를 인쇄한 경우
+                    break; // 반복문을 빠져나감
+                } else { // 요청한 문서가 아닌 경우
+                    location--; // 다음에 처리할 요청한 문서의 위치를 하나 감소시킴
+                }
+            } else { // 현재 문서의 우선순위가 가장 높은 것이 아닌 경우
+                queue.add(priority); // 대기열의 맨 뒤로 다시 추가함
+                if (location == 0) { // 요청한 문서의 위치인 경우
+                    location = queue.size() - 1; // 다음에 처리할 요청한 문서의 위치를 대기열의 마지막으로 설정함
+                } else { // 요청한 문서가 아닌 경우
+                    location--; // 다음에 처리할 요청한 문서의 위치를 하나 감소시킴
                 }
             }
-            if(flag) { 
-                queue.add(cur);
-            }else{
-                now++;
-                if(cur.location == location) {
-                    answer = now;
-                    break;
-                }
-
-            }
         }
-        return answer;
+
+        return answer; // 인쇄한 문서 수 반환
     }
 }
