872f9fd3e09268573b21690323425f50d7f312f9
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-03-21 11:19:54 +0900

    [level 0] Title: 배열 자르기, Time: 3.45 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -1,7 +1,7 @@
-import java.util.*;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        return Arrays.copyOfRange(numbers, num1, num2 + 1);
+        return IntStream.rangeClosed(num1, num2).map(i -> numbers[i]).toArray();
     }
 }
