725f81ae4fb89c620b888c79b7b0c0ae7ea63dcc
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-03-24 14:07:03 +0900

    Update 소수 찾기.java

--- 백준/Silver/1978. 소수 찾기/소수 찾기.java
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -4,17 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         int N = sc.nextInt();
-
         int count = 0;
-
         for(int i = 0; i < N; i++) {
-
             boolean isPrime = true;
-
             int num = sc.nextInt();
-
             if(num == 1) {	
                 continue;
             }
