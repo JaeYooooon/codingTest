e2aab27b86bafb8cca0624e8db3117841f258294
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-03-24 14:06:01 +0900

    Update 행성 X3.java

--- 백준/Gold/2830. 행성 X3/행성 X3.java
+++ 백준/Gold/2830. 행성 X3/행성 X3.java
@@ -10,12 +10,11 @@ public class Main {
         for (int i = 0; i < n; i++) {
             nums[i] = Long.parseLong(br.readLine());
         }
-        // 자리수가 작은 것을 기준으로 정렬
         Arrays.sort(nums);
-        for (int i = 0; i < 63; i++) { // 최대 63비트
+        for (int i = 0; i < 63; i++) { 
             long cnt = 0;
             for (int j = 0; j < n; j++) {
-                if ((nums[j] & (1L << i)) != 0) { // i번째 자리가 1인 경우
+                if ((nums[j] & (1L << i)) != 0) { 
                     cnt++;
                 }
             }
