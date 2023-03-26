a499371e5cea719b746458dfe771ee1bd76e46dc
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-03-26 17:51:09 +0900

    Update 셀프 넘버.java

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-
         boolean[] check = new boolean[10001];	
         for (int i = 1; i < 10001; i++){
             int n = d(i);
