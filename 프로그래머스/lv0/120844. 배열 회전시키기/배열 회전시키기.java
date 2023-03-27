db3ca876f778f96670fd216cd2930063a8f810e8
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-03-27 14:30:34 +0900

    Update 배열 회전시키기.java

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -1,7 +1,6 @@
 class Solution {
     
     public int[] solution(int[] numbers, String direction) {
-        
        int[] answer = new int[numbers.length];
         for (int i = 0; i < numbers.length; i++){
             if(direction.equals("right")){
