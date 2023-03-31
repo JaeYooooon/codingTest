974efff99ed73be4cfd8e83654a5ae6452aea90b
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-04-01 01:48:58 +0900

    Update 푸드 파이트 대회.java

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -5,6 +5,7 @@ class Solution {
         for (int i = 1; i < food.length; i++) {
             sb.append(Integer.toString(i).repeat(food[i] / 2));
         }
+        
         return sb.toString() + "0" + sb.reverse().toString();
     }
 
