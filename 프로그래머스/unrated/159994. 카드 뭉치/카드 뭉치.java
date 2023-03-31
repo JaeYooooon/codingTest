c7d765feea914837984de3f8de25dfccbf75d647
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-04-01 01:48:05 +0900

    Update 카드 뭉치.java

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.java
@@ -5,6 +5,7 @@ import java.util.Queue;
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         String answer = "";
+        
         Queue<String> queue1 = new LinkedList<>(Arrays.asList(cards1));
         Queue<String> queue2 = new LinkedList<>(Arrays.asList(cards2));
         for(String text : goal){
