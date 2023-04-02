0c48050d356ce9b554e17bc8e4779e3eb851a768
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-04-02 22:30:21 +0900

    Update 최고의 집합.java

--- 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.java
@@ -1,14 +1,12 @@
 class Solution {
     public int[] solution(int n, int s) {
         int[] answer = new int[n];
-
         int cnt = n;
         for(int i = 0; i < n; i++) {
             answer[i] = s / cnt;
             s -= s / cnt;
             cnt--;
         }
-
         if(answer[0] == 0) {
             answer = new int[]{-1};
         }
