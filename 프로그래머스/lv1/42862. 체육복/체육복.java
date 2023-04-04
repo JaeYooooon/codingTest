3ffb84f3c3cb5dd67c374dd6cd88b2c5c773b557
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-04-04 17:30:59 +0900

    Update 체육복.java

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -3,8 +3,7 @@ class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = n - lost.length;      
         Arrays.sort(lost);
-        Arrays.sort(reserve);
-        
+        Arrays.sort(reserve);        
         for(int i = 0; i < lost.length; i++){
             for(int j = 0; j < reserve.length; j++){
                 if(lost[i] == reserve[j]){
