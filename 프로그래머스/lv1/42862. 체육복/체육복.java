e680eef859d7af98d2aacdb1448a4bdfc5df4ce1
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-04-04 17:30:00 +0900

    [level 1] Title: 체육복, Time: 0.45 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -6,9 +6,8 @@ class Solution {
         Arrays.sort(lost);
         Arrays.sort(reserve);
         
-        // 여벌 체육복을 가져온 학생이 도난당한 경우
-        for(int i=0; i<lost.length; i++){
-            for(int j=0; j<reserve.length; j++){
+        for(int i = 0; i < lost.length; i++){
+            for(int j = 0; j < reserve.length; j++){
                 if(lost[i] == reserve[j]){
                     answer++;
                     lost[i] = -1;
@@ -17,10 +16,10 @@ class Solution {
                 }
             }
         }
-        // 도난당한 학생에게 체육복 빌려주는 경우
-        for(int i=0; i<lost.length; i++){
-            for(int j=0; j<reserve.length; j++){
-                if((lost[i]-1 == reserve[j]) || (lost[i]+1 == reserve[j])){
+        
+        for(int i = 0; i < lost.length; i++){
+            for(int j = 0; j < reserve.length; j++){
+                if((lost[i] - 1 == reserve[j]) || (lost[i] + 1 == reserve[j])){
                     answer++;
                     reserve[j] = -1; 
                     break; 
