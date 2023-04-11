89a2b0f9d78bee91a162082db7a85cb533e80131
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-04-11 20:18:45 +0900

    Update 입국심사.java

--- 프로그래머스/lv3/43238. 입국심사/입국심사.java
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.java
@@ -9,7 +9,7 @@ class Solution {
         
         long mid ;
         long sum = 0; 
-        while(start <= end) {
+        while(start <= end) { 
             mid = (start + end) / 2;
             sum = 0;
             for(int time : times) { 
