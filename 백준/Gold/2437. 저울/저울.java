cad2544aab9aa78ed6b642b355c73bfbc5f25509
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-04-15 21:21:55 +0900

    Update 저울.java

--- 백준/Gold/2437. 저울/저울.java
+++ 백준/Gold/2437. 저울/저울.java
@@ -11,7 +11,7 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine(), " ");
-        int[] arr = new int[N];
+        int[] arr = new int[N]; 
         
         for (int i = 0; i < N; i++) { 
             arr[i] = Integer.parseInt(st.nextToken()); 
