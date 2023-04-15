0498969251dabb6cb07754e13f99e6ceee4e80dd
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-04-15 21:21:59 +0900

    Update 저울.java

--- 백준/Gold/2437. 저울/저울.java
+++ 백준/Gold/2437. 저울/저울.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken()); 
         st = new StringTokenizer(br.readLine(), " ");
         int[] arr = new int[N]; 
         
