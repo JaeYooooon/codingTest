61f1a768a35af1d982b12465977173741d047fa8
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-04-25 11:35:33 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 216 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] heights = new int[9];
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            heights[i] = sc.nextInt();
+            sum += heights[i];
+        }
+        Arrays.sort(heights);
+        for (int i = 0; i < 8; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - heights[i] - heights[j] == 100) { 
+                    for (int k = 0; k < 9; k++) {
+                        if (k == i || k == j) continue; 
+                        System.out.println(heights[k]);
+                    }
+                    return; 
+                }
+            }
+        }
+    }
+}
