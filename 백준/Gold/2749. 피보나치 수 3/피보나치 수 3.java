d4fcc298e0f46a7a9e2aed848c8ffd7870a66faf
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-04-25 10:43:47 +0900

    [Gold II] Title: 피보나치 수 3, Time: 236 ms, Memory: 29560 KB -BaekjoonHub

---
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int pisano = 1500000;
+        long n = sc.nextLong() % pisano;
+        long[] arr = new long[pisano];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i = 2; i < pisano; i++){
+            arr[i] = (arr[i - 1] + arr[i - 2]) % 1000000;
+        }
+        System.out.println(arr[(int)n]);
+    }
+}
