fd22be627a0af2e8c7c1cb69cfb972023ee5b4c6
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-05-08 21:47:17 +0900

    Update 숫자의 표현.java

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,7 +1,6 @@
 class Solution {
       public int solution(int n) {
           int answer = 0;
-          
           for(int i = 1; i <= n; i++) {
               int sum = 0;
               for(int j = i; j <= n; j++) {
