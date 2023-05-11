06c4aed1d8ce07dc944eb5b4eab03387e5e99802
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-05-11 20:27:29 +0900

    Update 피보나치 수 3.java

--- 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
@@ -4,8 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int pisano = 1500000;
-        long n = sc.nextLong() % pisano;
+        long n = sc.nextLong() % pisano;  
         long[] arr = new long[pisano];
+        
         arr[0] = 0;
         arr[1] = 1;
         for(int i = 2; i < pisano; i++){
