908bc826a2080dbf6e54f8e794dbaedc4349c43c
JaeYooooon <99658884+JaeYooooon@users.noreply.github.com>
2023-09-20 16:25:09 +0900

    Update 카드2.java

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -21,10 +21,10 @@ public class Main {
 		
 		
 		while(q.size() > 1) {
-			q.poll();	// 맨 앞의 원소 버림 
-			q.offer(q.poll());	// 맨 앞의 원소를 버림과 동시에 버려진 원소를 맨 뒤에 삽입 
+			q.poll();	
+			q.offer(q.poll());	
 		}
 		
-		System.out.println(q.poll());	// 마지막으로 남은 원소 출력 
+		System.out.println(q.poll());	
 	}
 }
